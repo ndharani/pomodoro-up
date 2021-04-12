09a0ff759f5503412fe048de32e06bdac106cf0d
Nikhat Dharani <nikhat@everquote.com>
2021-04-11 21:19:18 -0500

    Remove special logic needed for Github Codespaces

--- pomodoro_up/app/controllers/application_controller.rb
+++ pomodoro_up/app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
-  # default throws exceptions. this line prevents exceptions from being thrown
-  # TODO: is there a better workaround to this error
-    # "HTTP Origin header (http://localhost) didn't match request.base_url (https://ndharani-pomodoro-up-ph5x-3000.githubpreview.dev)"
-  protect_from_forgery
-
   helper_method :errors, :current_user, :logged_in?
 
   def errors
--- pomodoro_up/db/seeds.rb
+++ pomodoro_up/db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+users = [
+    {
+        username: 'u1',
+        email: 'u1@a.com',
+        password: 'p1',
+    },
+    {
+        username: 'u2',
+        email: 'u2@a.com',
+        password: 'p2',
+    },
+    {
+        username: 'u3',
+        email: 'u3@a.com',
+        password: 'p3',
+    },
+]
+User.create(users)
