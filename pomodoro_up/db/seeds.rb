181fe73f73ed0d50ac4652dc1602cae83ceae254
Nikhat Dharani <nikhat@everquote.com>
2021-04-13 19:44:11 -0500

    Show tasks on task list page

--- pomodoro_up/db/seeds.rb
+++ pomodoro_up/db/seeds.rb
@@ -26,11 +26,18 @@ users = [
 ]
 User.create(users)
 
-# Create task lists
+# Create task lists & tasks
 u1 = User.find(1)
-u1.task_lists.create(name: "Daily Todo")
+tl1 = u1.task_lists.create(name: "Daily Todo")
+tl2 = u1.task_lists.create(name: "Weekly Todo")
+tl3 = u1.task_lists.create(name: "Monthly Todo")
+task_3_1 = tl3.tasks.create(name: "Sprint planning")
+task_3_2 = tl3.tasks.create(name: "Run retro")
 
 u2 = User.find(2)
-u2.task_lists.create(name: "4/19 Todo")
-u2.task_lists.create(name: "4/20 Todo")
-u2.task_lists.create(name: "4/21 Todo")
+tl4 = u2.task_lists.create(name: "4/19 Todo")
+tl5 = u2.task_lists.create(name: "4/20 Todo")
+tl6 = u2.task_lists.create(name: "4/21 Todo")
+task_6_1 = tl6.tasks.create(name: "Hard deletes planning")
+task_6_2 = tl6.tasks.create(name: "French practice")
+task_6_3 = tl6.tasks.create(name: "Bake vanilla cake")
