455a397ced6a9e5f29a7a0a665e0c7147a9a65de
Nikhat Dharani <nikhat@everquote.com>
2021-04-16 07:38:49 -0500

    Define task status types in the model

--- pomodoro_up/app/controllers/task_lists_controller.rb
+++ pomodoro_up/app/controllers/task_lists_controller.rb
@@ -12,6 +12,7 @@ class TaskListsController < ApplicationController
         @task_list = TaskList.find_by(id: params[:id], user_id: current_user.id)
         if @task_list.nil?
             render "not_found"
+            return
         end
 
         @active_tasks = @task_list.tasks.where.not(status: @deleted_task_status).all
--- pomodoro_up/app/models/task_list.rb
+++ pomodoro_up/app/models/task_list.rb
@@ -1,4 +1,16 @@
 class TaskList < ApplicationRecord
     belongs_to :user
     has_many :tasks, dependent: :destroy  # remove associated tasks when removing a task list
+
+    def active_tasks
+      self.tasks.where(status: Task.active_task_status).all
+    end
+
+    def finished_tasks
+      self.tasks.where(status: Task.finished_task_status).all
+    end
+
+    def deleted_tasks
+      self.tasks.where(status: Task.deleted_task_status).all
+    end
 end
